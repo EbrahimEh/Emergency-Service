14e7121d546c1807079aac60f72311b55c05e511
EbrahimEh <mreh017@gmail.com>
2025-08-28 20:13:22 +0600

    Complete All Functions Without Responsive

--- js/emergency.js
+++ js/emergency.js
@@ -1,26 +1,27 @@
 //
 
-
+//History
+const historyData = [];
 
 //Heart Clicked
 let counting = 0;
 
 const container = document.getElementById('card-container');
 
-container.addEventListener('click', function(e){
-    if(e.target.classList.contains('heart-clicked')){
+container.addEventListener('click', function (e) {
+    if (e.target.classList.contains('heart-clicked')) {
         counting++;
         document.getElementById('heart-count').innerText = counting;
     }
 })
 
 
-//Coins Counting
+//Coins Counting + history 
 let coins = 100;
 
-container.addEventListener('click', function(e){
-    if(e.target.classList.contains('call-btn') || e.target.closest('.call-btn')){
-        if(coins >= 20){
+container.addEventListener('click', function (e) {
+    if (e.target.classList.contains('call-btn') || e.target.closest('.call-btn')) {
+        if (coins >= 20) {
             coins -= 20;
 
             //parent div call btn
@@ -28,34 +29,75 @@ container.addEventListener('click', function(e){
 
             let title = callbtnParent.querySelector('.card-title').innerText;
             let cardNum = callbtnParent.querySelector('.card-num').innerText;
-            
+
             document.getElementById('coin').innerText = coins;
 
-            alert("Calling " + title + " " + cardNum)
+            alert("📞 Calling " + title + " " + cardNum)
+
+            //history
+            let date = new Date().toLocaleTimeString();
+
+            const div = document.createElement('div');
+            const historyContainer = document.getElementById('history-Container');
+
+            div.innerHTML = `
+            <div class="flex gap-5 text-xl rounded-lg mt-5 bg-[#FAFAFA] p-3 items-center">
+                        <div class="flex-2 font-semibold">
+                            <h2>${title}</h2>
+                            <p>${cardNum}</p>
+                        </div>
+                        <div class="font-semibold">${date}</div>
+                    </div>
+            `
+            historyContainer.appendChild(div)
         }
-        else{
-            alert("Not Working!")
+        else {
+            alert("❌ You are out of coins! You will need at least 20 coins to make a call.")
         }
     }
 })
-// const callBtn = document.getElementsByClassName('call-btn');
 
-// //
-// for(let i = 0; i < callBtn.length; i++){
-//     callBtn[i].addEventListener('click', function(){
-//         if(coins >= 20){
-            
-//             coins = coins - 20;
-//             let cardParent = this.parentElement;
+//History Clear Part
+document.getElementById('history-clear')
+    .addEventListener('click', function (e) {
+        document.getElementById('history-Container').innerHTML = '';
 
-//             let title = cardParent.getElementsByClassName('card-title')[0].innerText;
-//             let number = cardParent.getElementsByClassName('card-num')[0].innerText;
+    })
 
-//             alert("Calling" + title, number);
-//             document.getElementById('coin').innerText = coins;
-//         }
-//         else{
-//             alert("sorry!")
-//         }
-//     })
-// }
+
+//copy number
+let copy = 0;
+container.addEventListener('click', function (e) {
+    // const copyButton = document.getElementById('copy-button');
+
+    if (e.target.classList.contains('copy-button') || e.target.closest('.copy-button')) {
+        copy++;
+
+        //apend
+        document.getElementById('copy-counting').innerText = copy;
+
+        // parentdiv
+        let parentContainer = e.target.closest('.callBtn-container');
+        if (parentContainer) {
+            //card num
+            let cardNumbers = parentContainer.querySelector('.copy-num').innerText;
+
+            //copy
+            navigator.clipboard.writeText(cardNumbers).then(() => {
+                alert("Copied Succesfully: " + cardNumbers);
+            }).catch(errors => {
+                console.error("Copy Failed", errors)
+            })
+        }
+
+
+    }
+})
+// const nu = document.querySelector('.copy-num').innerText;
+
+// console.log('num', nu)
+
+
+
+
+
